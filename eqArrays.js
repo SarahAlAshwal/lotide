46a999d28083d784a15a45ea0c39c0598b22eecd
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-18 19:31:56 +0000

    adding eqArrays.js file

---
+++ eqArrays.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  let passEmoji = String.fromCodePoint(0x1F44C);
+  let failEmoji = String.fromCodePoint(0x1F4A5);
+
+  if (typeof actual === "string") {
+
+    actual = `"${actual}"`;
+    
+  }
+
+  if (typeof expected === 'string') {
+    expected = `"${expected}"`;
+  }
+
+  if (actual === expected) {
+    console.log(`${passEmoji} Assertion passed: ${actual} == ${expected}`);
+  } else {
+    console.log(`${failEmoji} Assertion failed: ${actual} != ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  let n1 = arr1.length;
+  let n2 = arr2.length;
+  if (n1 !== n2) {
+    return false;
+  }else{
+    for( let i = 0; i < n1; i++) {
+      if(arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true; 
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false) // => false
