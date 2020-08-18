37c7fe065b8fea135ddc8f93bad511a0fcda72df
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-18 19:56:42 +0000

    adding assertArraysEqual.js to the project

--- eqArrays.js
+++ eqArrays.js
@@ -25,18 +25,18 @@ const eqArrays = function(arr1, arr2) {
   let n2 = arr2.length;
   if (n1 !== n2) {
     return false;
-  }else{
-    for( let i = 0; i < n1; i++) {
-      if(arr1[i] !== arr2[i]) {
+  } else {
+    for (let i = 0; i < n1; i++) {
+      if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
-    return true; 
+    return true;
   }
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
