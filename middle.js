332f4f0c0c1ffea2ab928114d8a47b741e5e00f4
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-23 06:01:29 +0000

    refactoring middle and creatin mdiddleTest

--- middle.js
+++ middle.js
@@ -1,28 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  let n1 = arr1.length;
-  let n2 = arr2.length;
-  if (n1 !== n2) {
-    return false;
-  } else {
-    for (let i = 0; i < n1; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  let passEmoji = String.fromCodePoint(0x1F44C);
-  let failEmoji = String.fromCodePoint(0x1F4A5);
-
-  if (eqArrays(firstArray, secondArray)) {
-    console.log(`${passEmoji} Assertion passed: [${firstArray}] == [${secondArray}]`);
-  } else {
-    console.log(`${failEmoji} Assertion failed: [${firstArray}] != [${secondArray}]`);
-  }
-};
+const eqArrays = require ('./eqArrays');
+const assertArraysEqual = require ('./assertArraysEqual');
 
 const middle = function(array) {
   let mid = [];
@@ -38,12 +15,5 @@ const middle = function(array) {
   return mid;
 };
 
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle([]),[]);
-assertArraysEqual(middle(["1","Hello","Lighthouse"]),["Hello"]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8,9]),[5]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,10]),[5,6 ]);
-assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
+module.exports = middle;
+
